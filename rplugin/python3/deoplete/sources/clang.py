82a7008ce8dbdfb85f6c8e7db864ba3c4c729d66
Koichi Shiraishi <k@zchee.io>
2016-02-16 04:10:33 +0900

    Add try...except import clang-python3
    Signed-off-by: Koichi Shiraishi <k@zchee.io>

--- rplugin/python3/deoplete/sources/clang.py
+++ rplugin/python3/deoplete/sources/clang.py
@@ -1,8 +1,21 @@
+import os
+import sys
+
 from .base import Base
 
 from deoplete.util import charpos2bytepos
 from deoplete.util import debug
 from deoplete.util import error
+
+try:
+  current_dir = os.path.dirname(os.path.abspath(__file__))
+  clang_dir = os.path.join(os.path.dirname(current_dir), 'clang')
+  sys.path.insert(0, clang_dir)
+  from clang.cindex import *
+except ImportError:
+    # TODO(zchee) Not use 'import neovim'. instead of deoplete.error
+    import neovim
+    neovim.Nvim.command("echoerr 'cant find clang-python3 module'")
 
 class Source(Base):
 
