c471b3a9c80bb9a5d2e1c121b57bb729d10660b9
Koichi Shiraishi <k@zchee.io>
2016-02-16 05:29:27 +0900

    Rename source file caused by can't jedi#goto
    - jedi can not load same file name module
    
    Signed-off-by: Koichi Shiraishi <k@zchee.io>

--- rplugin/python3/deoplete/sources/deoplete-clang.py
+++ rplugin/python3/deoplete/sources/deoplete-clang.py
@@ -1,5 +1,5 @@
-import os
 import sys
+import clang.cindex
 
 from .base import Base
 
@@ -8,10 +8,10 @@ from deoplete.util import debug
 from deoplete.util import error
 
 try:
-  current_dir = os.path.dirname(os.path.abspath(__file__))
-  clang_dir = os.path.join(os.path.dirname(current_dir), 'clang')
-  sys.path.insert(0, clang_dir)
-  from clang.cindex import *
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    clang_dir = os.path.join(os.path.dirname(current_dir), 'clang')
+    sys.path.insert(0, clang_dir)
+    import clang.cindex as clang
 except ImportError:
     # TODO(zchee) Not use 'import neovim'. instead of deoplete.error
     import neovim
@@ -29,7 +29,7 @@ class Source(Base):
         self.rank = 500
 
     def get_complete_position(self, context):
-        return
+        return -1
 
     def gather_candidates(self, context):
-        return
+        return []
